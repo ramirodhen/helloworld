47e7d815fe858f71ca7ea72d9f0844b4932f4b74
Ramiro Rodríguez <rrodriguezhenriquez@icloud.com>
2026-01-10 20:25:02 +0000

    correccion de errores

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -55,14 +55,12 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "4.0", "ERROR ADD"
         )
 
-   def test_divide_by_zero_returns_406(self):
-    url = f"{BASE_URL}/calc/divide/10/0"
-    response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-    self.assertEqual(
-        response.status,
-        http.client.NOT_ACCEPTABLE,
-        f"Error en la petición API a {url}"
-    )
+    def test_divide_by_zero_returns_406(self):
+        url = f"{BASE_URL}/calc/divide/10/0"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.NOT_ACCEPTABLE, f"Error en la petición API a {url}"
+        )
 
 '''
     def test_api_sqrt(self):
