38170442907aa06c5d208bb97667b21e624b6dc7
Ramiro Rodríguez <rrodriguezhenriquez@icloud.com>
2026-01-16 22:15:34 +0000

    Creamos test para cubrir 100% cobertura de pruebas

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -61,6 +61,12 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
+
+    def test_divide_by_zero_raises_typeerror(self):
+        self.assertRaises(TypeError, self.calc.divide, 10, 0)
+
+    def test_divide_raises_typeerror_when_y_is_not_number(self):
+        self.assertRaises(TypeError, self.calc.divide, 10, "2")
         
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
